4155138ccb38012b015f8834933aa5fc6a585b3b
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2020-12-18 22:33:51 +0100

    fix: exception handler for Model not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException)
+        {
+            return Response::make('Not found',404);
+        }
         return parent::render($request, $exception);
     }
 }
